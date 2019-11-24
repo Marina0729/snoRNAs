9c17635104fb336384118361a5e122d570ef091b
Alexander <ale097@csiro.au>
2019-11-24 18:42:40 +1100

    Initial commit

---
+++ scripts/snoRNAsBS.R
@@ -0,0 +1,7 @@
+.libPaths(c("C:/Users/ale097/Data School/Packages"))
+library(tidyverse)
+
+ribometh <- read_csv("data/Erales_2017_ribomethseq.csv")
+
+snoRNAs <- read_csv("data/Jorjani_2016_snoRNA_atlas.csv")
+
